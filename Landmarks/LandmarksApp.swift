535f40608680146dabde2a4aa6b9ba3d37120166
hannehaugland <hsofiehaugland@gmail.com>
2024-06-17 21:35:32 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hanne Sofie Haugland on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
